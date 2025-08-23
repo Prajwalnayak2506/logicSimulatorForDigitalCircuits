18e423b38a82583d325daed709b1d732c44e6c1b
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-08-24 00:41:14 +0530

    graphing done

--- graphiphy.cpp
+++ graphiphy.cpp
@@ -3,40 +3,40 @@
 #include <string>
 #include "node.h"
 using namespace std;
-void printGraph(unordered_map<int, Node*>& circuitMap) {
-    int node_count = 0;
-    for (auto& kv : circuitMap) {
-        cout<<"node counter : "<<node_count<<"\n";
-        Node* gate = kv.second;
-        cout << "Node ID: " << gate->id << "\n";
-        cout << "Type: " << gate->type << "\n";
-        cout << "Output: " << gate->output << "\n";
-        cout << "Inputs: ";
-        for (int in : gate->inputs) {
-            cout << in << " ";
-        }
-        cout<<endl;
-        node_count++;
-        if (gate->parents.empty()) {
-            cout << "None";
-        } else {
-            cout<<"parents are: ";
-            for (Node* parent : gate->parents) {
-                cout << parent->id << " ";
-            }
-        }
-        if (gate->children.empty()) {
-            cout << "None";
-        } else {
-            cout<<"children are: ";
-            for (Node* children : gate->children) {
-                cout << children->id << " ";
-            }
-        }
-        cout << endl;
-        cout << "\n----------------------\n";
-    }
-}
+// void printGraph(unordered_map<int, Node*>& circuitMap) {
+//     int node_count = 0;
+//     for (auto& kv : circuitMap) {
+//         cout<<"node counter : "<<node_count<<"\n";
+//         Node* gate = kv.second;
+//         cout << "Node ID: " << gate->id << "\n";
+//         cout << "Type: " << gate->type << "\n";
+//         cout << "Output: " << gate->output << "\n";
+//         cout << "Inputs: ";
+//         for (int in : gate->inputs) {
+//             cout << in << " ";
+//         }
+//         cout<<endl;
+//         node_count++;
+//         if (gate->parents.empty()) {
+//             cout << "None";
+//         } else {
+//             cout<<"parents are: ";
+//             for (Node* parent : gate->parents) {
+//                 cout << parent->id << " ";
+//             }
+//         }
+//         if (gate->children.empty()) {
+//             cout << "None";
+//         } else {
+//             cout<<"children are: ";
+//             for (Node* children : gate->children) {
+//                 cout << children->id << " ";
+//             }
+//         }
+//         cout << endl;
+//         cout << "\n----------------------\n";
+//     }
+// }
 void buildGraph(unordered_map<int,Node*>& circuitMap,vector<Node*> OverallOutputs){
     for (auto& gate_value : circuitMap){
         Node* gate = gate_value.second;
@@ -55,5 +55,7 @@ for (int i=0;i<OverallOutputs.size();i++){
     gate->parents.push_back(circuitMap[in]);
     circuitMap[in]->children.push_back(gate);
 }}
-printGraph(circuitMap);
+//testing
+// printGraph(circuitMap);
+
 }
--- graphiphy.h
+++ graphiphy.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <unordered_map>
 #include "node.h"
-void printGraph(std::unordered_map<int, Node*>& circuitMap);
+// void printGraph(std::unordered_map<int, Node*>& circuitMap);
 void buildGraph(std::unordered_map<int, Node*>& circuitMap,vector<Node*> OverallOutputs);
